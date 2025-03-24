8ad9d429a8262c54f14524f029b9708e98f8c1ea
Walter-Vanegas <walter.vanegas@coxautoinc.com>
2025-03-24 16:17:38 -0500

    fix

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -15,7 +15,7 @@ const Chat = () => {
     _messages = [..._messages, { text, sender: "user" },  { text: 'Procesando...', sender: "bot" }]; 
     setMessages(_messages);
     
-    fetch("https://pockiactionloadbalancer-1198368912.us-east-1.elb.amazonaws.com/postdata", {
+    fetch("http://pockiactionloadbalancer-1198368912.us-east-1.elb.amazonaws.com/postdata", {
       method: "POST",
       headers: {
           "Content-Type": "application/json"
